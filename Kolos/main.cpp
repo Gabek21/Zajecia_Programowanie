d22cf998bf415bd64d8473df0aa9f9bd8dcf11e5
Pawel <gabek@vp.pl>
2012-01-19 22:50:05 +0100

    przetlumaczone z c

---
+++ Kolos/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+
+using namespace std;
+
+struct Address {
+    int id;
+    int set;
+    char name[MAX_DATA]
+    char email[MAX_DATA]
+};
+
+struct Database
+{
+    struct Address_rows[MAX_ROWS]
+}
+
+struct Connection
+{
+    // FILE * file;
+    Databse *db;
+}
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
